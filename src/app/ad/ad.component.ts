56c2112a0da25956f86577911c28cf378e8d9616
Purple Capacitor <mihailom@protonmail.com>
2020-09-17 17:20:20 +0200

    Rework on ad page and profile setup.

--- src/app/ad/ad.component.ts
+++ src/app/ad/ad.component.ts
@@ -1,4 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Apollo, gql} from 'apollo-angular';
+import {Router} from '@angular/router';
+import {ConsoleLogger} from '@angular/compiler-cli/ngcc';
+
+
+const placeAd = gql`
+  mutation PlaceAd($input: AdsInput, $carModel: CarModelInput,
+    $user: UserInput,
+    $extras: ExtrasInput,
+    $condition: ConditionInput,
+    $characteristics: CharacteristicsInput
+    $safety: SafetyInput) {
+    placeAd(input: $input, user: $user, extras: $extras, carModel: $carModel,
+      condition: $condition, characteristics: $characteristics, safety: $safety) {
+      id
+    }
+  }
+`;
 
 @Component({
   selector: 'app-ad',
@@ -7,12 +25,79 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdComponent implements OnInit {
 
-  constructor() { }
+  private user = JSON.parse(localStorage.getItem('currentUser'));
+
+  constructor(
+    private apollo: Apollo,
+    private router: Router
+  ) {
+  }
 
   ngOnInit(): void {
   }
 
-  placeAd(value: any) {
-    
+  placeAd(values: any) {
+    let input = {
+      description: values['description'],
+      address: values['address'],
+      phone: values['phone'],
+      price: values['price']
+    };
+    let user = {
+      username: this.user.username
+    };
+    let extras = {
+      cruiseControl: values['cruiseControl'],
+      electricalMirrors: values['electricalMirrors'],
+      electricalSeats: values['electricalSeats'],
+      electricalWindows: values['electricalWindows'],
+      multifunctionalSteeringWheel: values['multifunctionalSteeringWheel'],
+      bluetooth: values['bluetooth'],
+      ledHeadlights: values['ledHeadlights'],
+      heatedSeats: values['heatedSeats']
+    };
+    let carModel = {
+      make: values['make'],
+      model: values['model'],
+      year: values['year'].substring(0, 4),
+      kilometers: values['km'],
+      displacement: values['displacement'],
+      hp: values['hp']
+    };
+    let condition = {
+      firstOwner: values['firstOwner'],
+      serviceHistory: values['serviceHistory'],
+      spareKey: values['spareKey'],
+      taxi: values['taxi']
+    };
+    let characteristics = {
+      emissionClass: values['emissionClass'],
+      ac: values['ac'],
+      drivetrain: values['drivetrain'],
+      color: values['color'],
+      registeredUntil: values['registeredUntil']
+    };
+    let safety = {
+      ABS: values['abs'],
+      ESP: values['esp'],
+      airbags: values['airbags'],
+      centralLocking: values['centralLocking'],
+      childLock: values['childLock']
+    };
+
+    this.apollo.mutate({
+      mutation: placeAd,
+      variables: {
+        input: input,
+        user: user,
+        carModel: carModel,
+        extras: extras,
+        condition: condition,
+        characteristics: characteristics,
+        safety: safety
+      }
+    }).subscribe( ({data}) => {
+      this.router.navigate(['/profile']);
+    })
   }
 }
--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,5 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from '@angular/router';
+import {Apollo, gql} from 'apollo-angular';
+
+const userAds = gql`
+    query UserAds($username: String!) {
+      userAds(username: $username) {
+        id
+        price
+        carModel {
+          make
+          model
+          year
+        }
+        characteristics {
+            color
+        }
+      }
+    }
+`
+
+const deleteAd = gql`
+    mutation DeleteAd($id: Int!) {
+      deleteAd(id: $id)
+    }
+`
 
 @Component({
   selector: 'app-profile',
@@ -8,14 +32,41 @@ import {Router} from '@angular/router';
 })
 export class ProfileComponent implements OnInit {
 
+  public ads = [];
+  private user = JSON.parse(localStorage.getItem('currentUser'));
+
   constructor(
     private router: Router,
+    private apollo: Apollo
   ) { }
 
   ngOnInit(): void {
+   this.fetchData()
   }
 
   newAd() {
     this.router.navigate(['/ads']);
+  }
+
+  deleteAd(id) {
+    this.apollo.mutate({
+      mutation: deleteAd,
+      variables: {
+        id: id
+      }
+    }).subscribe(({data}) => {
+      this.fetchData()
+    });
+  }
+
+  fetchData() {
+    this.apollo.mutate({
+      mutation: userAds,
+      variables: {
+        username: this.user.username
+      }
+    }).subscribe( ({data}) => {
+      this.ads = data['userAds'];
+    });
   }
 }
